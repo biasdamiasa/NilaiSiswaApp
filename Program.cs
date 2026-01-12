bfeb7c3277e2832bff782c4a0cc7627c5a83a3a4
biasdamiasa <bias@smktelkom-mlg.sch.id>
2026-01-12 09:45:58 +0700

    tambah input nama dan nilai

--- Program.cs
+++ Program.cs
@@ -2,3 +2,9 @@
 string nama;
 
 Console.WriteLine("Aplikasi Nilai Siswa");
+
+Console.Write("Input Nama: ");
+nama = Console.ReadLine();
+
+Console.Write("Input Nilai: ");
+nilai = int.Parse(Console.ReadLine());
